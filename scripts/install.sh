aa36709c52d1104509cc1541379fd3bf5d1c9b7f
bitrunner <mrflint46@gmail.com>
2026-03-30 22:19:12 +0200

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
-# This script provisions the system.
 
 set -euo pipefail
 
 # Install the bootloader theme
-# sudo cp -rT ~/.config/dotfiles/rose-pine /boot/EFI/refind/
+# sudo cp -rT ~/.config/dotfiles_hyprland/rose-pine /boot/EFI/refind/
 
-# My configs and scripts
-mkdir -p ~/.local/bin && ln -sfn ~/.config/dotfiles/scripts/* ~/.local/bin
-ln -sfn ~/.config/dotfiles/hypr ~/.config/hypr
-# ln -sfn ~/.config/dotfiles/mako ~/.config/mako
-ln -sfn ~/.config/dotfiles/dunst ~/.config/dunst
-ln -sfn ~/.config/dotfiles/waybar ~/.config/waybar
-ln -sfn ~/.config/dotfiles/wofi ~/.config/wofi
+# Install Hyprland environment
+mkdir -p ~/.local/bin && ln -sfn ~/.config/dotfiles_hyprland/scripts/* ~/.local/bin
+ln -sfn ~/.config/dotfiles_hyprland/hypr ~/.config/hypr
+# ln -sfn ~/.config/dotfiles_hyprland/mako ~/.config/mako
+ln -sfn ~/.config/dotfiles_hyprland/dunst ~/.config/dunst
+ln -sfn ~/.config/dotfiles_hyprland/waybar ~/.config/waybar
+ln -sfn ~/.config/dotfiles_hyprland/wofi ~/.config/wofi
 
 # Zsh
-ln -sfn ~/.config/dotfiles/.zshrc ~/.zshrc
+ln -sfn ~/.config/dotfiles_hyprland/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
-ln -sfn ~/.config/dotfiles/starship.toml ~/.config/starship.toml
+ln -sfn ~/.config/dotfiles_hyprland/starship.toml ~/.config/starship.toml
 
 source ~/.zshrc
